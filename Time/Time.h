e2d2f871bfc7c9cf3c26a27b8bd2a93adfb3236b
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 17:36:32 +0100

    Initial Commit

---
+++ Time/Time.h
@@ -0,0 +1,19 @@
+//
+//  Time.h
+//  Time
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Time.
+FOUNDATION_EXPORT double TimeVersionNumber;
+
+//! Project version string for Time.
+FOUNDATION_EXPORT const unsigned char TimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Time/PublicHeader.h>
+
+
